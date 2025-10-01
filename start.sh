a84042fc04c1383a1a316b01d3fb15bb6c3436d7
jadapache <dpacheco@unab.edu.co>
2025-10-01 10:11:44 -0500

    refactor: cambio comandos

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-set -e
-
-cd /var/www/metalsoft/miit_api
 
 # Instalación de dependencias
 pip install -r requirements.txt --no-cache-dir
 
 # Ejecutar la aplicación
 exec uvicorn main:app --host 0.0.0.0 --port 8443
+
