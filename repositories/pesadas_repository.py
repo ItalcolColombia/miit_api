3da50ae629cdcf3e8364090a8e171b69213a9124
jadapache <dpacheco@unab.edu.co>
2025-10-23 12:26:21 -0500

    chore: sum pesadas by tran

--- repositories/pesadas_repository.py
+++ repositories/pesadas_repository.py
@@ -6,7 +6,7 @@ from core.contracts.auditor import Auditor
 from repositories.base_repository import IRepository
 from schemas.pesadas_corte_schema import PesadasCalculate, PesadasCorteCreate, PesadasRange
 from schemas.pesadas_schema import PesadaResponse, VPesadasAcumResponse
-from database.models import Pesadas, Viajes, Flotas, Transacciones, Materiales, Bls, PesadasCorte
+from database.models import Pesadas, Viajes, Flotas, Transacciones, Materiales, Bls, PesadasCorte, VPesadasAcumulado
 
 
 class PesadasRepository(IRepository[Pesadas, PesadaResponse]):
@@ -16,6 +16,24 @@ class PesadasRepository(IRepository[Pesadas, PesadaResponse]):
         self.db = db
         super().__init__(model, schema, db, auditor)
 
+    async def get_sumatoria_pesada(self, puerto_ref: Optional[str] = None, tran_id: Optional[int] = None) -> Optional[VPesadasAcumResponse]:
+        """
+                Filter pesada sum
+
+                Returns:
+                    A sum of Pesadas register matching the filter, otherwise, returns a null.
+                """
+
+        query = select(VPesadasAcumulado)
+
+        if puerto_ref is not None:
+            query = query.where(VPesadasAcumulado.puerto_id == puerto_ref)
+
+        if tran_id is not None:
+            query = query.where(VPesadasAcumulado.transaccion == tran_id)
+
+        result = await self.db.execute(query)
+        return result.scalar_one_or_none()
 
     async def get_sumatoria_pesadas(self, puerto_ref: str, tran_id: Optional[int] = None) -> List[PesadasCalculate] | None:
         """
