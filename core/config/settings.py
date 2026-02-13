501ff32dfdab506b92df9c669aae888ddbc5271c
Daniel Castillo <daniel.castillo@metalteco.com>
2026-02-13 08:47:08 -0500

    chore: bump API version to 0.0.43

--- core/config/settings.py
+++ core/config/settings.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
     API_HOST: str = "0.0.0.0"
     API_PORT: int = "8000"
     API_V1_STR: str = "v1"
-    API_VERSION_NUM: str = "0.0.42"
+    API_VERSION_NUM: str = "0.0.43"
     API_LOG_LEVEL: str = "DEBUG"
     APP_LOG_DIR: str = "/var/www/metalteco/log/app_logs"
     ALLOWED_HOSTS: list[str] = [
