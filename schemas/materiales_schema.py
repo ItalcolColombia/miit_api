6e7807e6493859b234e698a5fd0cf1dd25eacc05
Daniel Castillo <daniel.castillo@metalteco.com>
2026-03-09 15:09:33 -0500

    feat: add scripts for logging adjustments and configuring direct dispatch

--- schemas/materiales_schema.py
+++ schemas/materiales_schema.py
@@ -1,18 +1,14 @@
 from datetime import datetime
-from decimal import Decimal
 from typing import Optional
 
 from pydantic import Field
 
 from schemas.base_schema import BaseSchema
 
-
 class MaterialesResponse(BaseSchema):
     id: int
     codigo : str
     nombre: str
-    tipo: str
-    densidad: Optional[Decimal] = None
     fecha_hora: Optional[datetime] = None
     usuario_id: Optional[int] = None
 
@@ -22,8 +18,6 @@ class MaterialesResponse(BaseSchema):
 class MaterialesCreate(BaseSchema):
     codigo: str = Field(..., max_length=10)
     nombre: str = Field(..., max_length=100)
-    tipo: str = Field(..., max_length=50)
-    densidad: Optional[Decimal] = Field(default=Decimal("0.60"), max_digits=4, decimal_places=2)
     fecha_hora: Optional[datetime] = None
     usuario_id: Optional[int] = None
 
@@ -32,16 +26,12 @@ class MaterialesCreate(BaseSchema):
             "example": {
                 "codigo": "TORSOYUSA",
                 "nombre": "TORTA DE SOYA USA",
-                "tipo" : "Ceral",
-                "densidad": 0.60
             }
         }
 
 class MaterialesUpdate(BaseSchema):
     codigo: Optional[str] = Field(None, max_length=10)
     nombre: Optional[str] = Field(None, max_length=100)
-    tipo: Optional[str] = Field(None, max_length=50)
-    densidad: Optional[Decimal] = Field(None, max_digits=4, decimal_places=2)
     fecha_hora: Optional[datetime] = None
     usuario_id: Optional[int] = None
 
